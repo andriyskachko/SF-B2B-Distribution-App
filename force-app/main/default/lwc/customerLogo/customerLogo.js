a2fb5d3e6a8fd52eaf97193b99aa0b3ab545f383
Andriy Skachko <96697847+andriyskachko@users.noreply.github.com>
2022-12-18 14:38:12 +0200

    reworked user logo to be menu with buttons

--- force-app/main/default/lwc/customerLogo/customerLogo.js
+++ force-app/main/default/lwc/customerLogo/customerLogo.js
@@ -1,32 +1,11 @@
-import { LightningElement, wire, api } from "lwc";
-import { getFieldValue, getRecord } from "lightning/uiRecordApi";
+import { LightningElement, wire } from "lwc";
 import { publish, MessageContext } from "lightning/messageService";
 import CUSTOMER_LOGGED_OUT from "@salesforce/messageChannel/CustomerLoggedOut__c";
-import NAME_FIELD from "@salesforce/schema/Account.Name";
 import "./customerLogo.css";
 
 export default class CustomerLogo extends LightningElement {
-  @api accountId = "";
-
-  @wire(getRecord, { recordId: "$accountId", fields: [NAME_FIELD] })
-  account;
-
   @wire(MessageContext)
   messageContext;
-
-  get accountName() {
-    return getFieldValue(this.account.data, NAME_FIELD);
-  }
-
-  get accountInitials() {
-    return this.accountName
-      ? this.accountName
-          .split(" ")
-          .slice(0, 2)
-          .map((word) => word[0])
-          .join("")
-      : "";
-  }
 
   handleLogout() {
     publish(this.messageContext, CUSTOMER_LOGGED_OUT);
